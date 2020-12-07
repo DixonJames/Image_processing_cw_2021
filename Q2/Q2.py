fa30c97f8504263c22b1d1ad21d6290ded1fb655
james <jamesmedixon@gmail.com>
2020-12-07 21:46:01 +0000

    motion blur  & gaussian noise on Q2

--- Q2/Q2.py
+++ Q2/Q2.py
@@ -3,7 +3,44 @@ import numpy as np
 import math
 import random
 
-class masks:
+def boundPixVal(val):
+    return max(0,min(255, val))
+
+class merg:
+    def colourDodge(self, imageA, imageB, pix_ratio):
+
+        width, height = imageA.shape
+        canvas = np.zeros((width,height), np.uint8)
+
+
+        for row in range(len(imageA)):
+            for col in range(len(imageA[0])):
+
+                if imageB[col, row] == 255:
+                    canvas[row][col] = 255
+                else:
+                    ratio = 255/(256 - imageB[row][col]) *pix_ratio
+                    if ratio < 0:
+                        print(ratio)
+                    else:
+                        canvas[row][col] = boundPixVal(imageA[row][col] * ratio)
+
+        return canvas
+
+class motion:
+    def line(self, size):
+        if size%2 == 0:
+            size += 1
+        canvas = np.zeros((size, size))
+
+        mid = size//2
+        for row in range(size):
+            canvas[row][row] = 1
+
+        return canvas
+
+
+class gaussian:
 
     def normalDis(self, x, sigma):
         return ((1 / (sigma * math.sqrt(2 * math.pi))) * (math.e ** (-(x ** 2 / (2 * (sigma ** 2))))))
@@ -17,14 +54,23 @@ class masks:
             out.append(self.normalDis(i * diff, sigma) * 1 )
         return out
 
-    def gausssianNoise(self, deviation, width):
+    def randomGaussianPixelChange(self, deviation, width):
+        """
+        :param deviation: sigma for nromal distribution
+        :param width:
+        :return:
+        """
         #dropoff = self.gaussianDegradation(20, deviation)
         distribution = [self.normalDis(0, deviation)]
         effect = [1]
+
+        max = self.normalDis(0, deviation)
+        distribution = [1]
+        effect = [self.normalDis(0, deviation)/max]
         for i in range(width):
-            distribution = [self.normalDis(1, deviation)] + distribution + [self.normalDis(1, deviation)]
-            effect = [self.normalDis(1, deviation)] + effect + [1+(1 - self.normalDis(1, deviation)) ]
-        return random.choice(effect, distribution, k = 1)
+            distribution = [self.normalDis(i+1, deviation)**2/max**2] + distribution + [self.normalDis(1+i, deviation)**2/max**2]
+            effect = [self.normalDis(i+1, deviation)/max] + effect + [(1+(1 - self.normalDis(i+1, deviation)/max))]
+        return random.choices(effect, distribution)
 
 
     def hypotenusePythag(self, sideA, sideB):
@@ -50,10 +96,21 @@ class masks:
         return mask
 
     def validatePoints(self, image, x, y):
-        if x > 0 and y > 0:
+        if x >= 0 and y >= 0:
             if x <= len(image[0])-1   and y <= len(image)-1:
                 return True
         return False
+
+    def gaussianNoise(self, image, intensity):
+        canvas = image.copy()
+        rows, cols = len(image), len(image[0])
+
+
+        for row in range(rows - 1):
+            for col in range(cols - 1):
+                canvas[row][col] = boundPixVal(int(image[row][col] * self.randomGaussianPixelChange(1/intensity, 10)[0]))
+        return canvas
+
 
 
     def applyMask(self, image, mask):
@@ -75,7 +132,7 @@ class masks:
                         trial_p_x = t_l_x + mask_col
                         trial_p_y = t_l_y - mask_row
 
-                        if self.validatePoints(image, trial_p_x, trial_p_y):
+                        if self.validatePoints(image, trial_p_x, trial_p_y) and mask[mask_row][mask_row] != 0:
                             sum_valid_mask += mask[mask_row][mask_col]
                             sum_valid_maskXintensity += mask[mask_row][mask_col] * image[trial_p_y][trial_p_x]
 
@@ -120,13 +177,21 @@ class sketch:
 if __name__ == '__main__':
 
     face_image = cv2.imread("face1.jpg")
-    print(masks().validatePoints(face_image, -1, -1))
+
     drawing = sketch(face_image)
 
     grey = drawing.greyscale
     inv_grey = drawing.greyscale_flip
 
-    blured_face = masks().applyMask(inv_grey, masks().gaussianMask(9, 21))
+    #blured_face = gaussian().applyMask(inv_grey, gaussian().gaussianMask(3, 1))
 
-    cv2.imwrite("test.jpg", grey)
-    cv2.imwrite("test_blur.jpg", blured_face)
+    #sketch_test = merg().colourDodge(grey, blured_face,0.75)
+
+
+    motion_blured = gaussian().applyMask(grey, motion().line(10))
+    noise_texture = gaussian().gaussianNoise(motion_blured, 0.05)
+
+    #sketch_test = cv2.divide(grey, 100-blured_face, scale=256)
+    cv2.imwrite("noise_test.jpg", noise_texture)
+    cv2.imwrite("motion_test.jpg", motion_blured)
+    #cv2.imwrite("test_blur.jpg", blured_face)
